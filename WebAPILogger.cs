150823a3e94d768c4f0114d4cd97556e2dde8380
evelyn sanchez <evestrange@yahoo.com>
2017-09-13 11:34:52 -0700

    webAPILogger

---
+++ WebAPILogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+using Sabio.Services.Interfaces;
+
+namespace Sabio.Services
+{
+   public class WebAPILogger :  ExceptionLogger
+    {
+        private readonly ILogger logger;
+        readonly IAuthenticationService authService;
+
+        public WebAPILogger(ILogger logger, IAuthenticationService authService)
+        {
+            this.logger = logger;
+            this.authService = authService;
+        }
+
+        public override void Log(ExceptionLoggerContext context)
+        {
+            var user = authService.GetCurrentUser();
+
+            logger.LogError(string.Format("Unhandled exception processing {0} for {1} {2}",
+                context.Request.Method,
+                context.Request.RequestUri,
+                context.Exception), user?.Id);
+
+            //null reference exception when no user is logged in
+           
+
+            
+
+        }
+    }
+}
